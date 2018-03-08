5285124af1c43dd5195c747b73277bd46bf35458
Mario Behrendt <m.behrendt@yanted.com>
2018-03-08 14:55:08 +0000

    Prevent tab from doing it's thing

--- src/Multiselect.vue
+++ src/Multiselect.vue
@@ -49,7 +49,7 @@
           @keydown.down.prevent="pointerForward()"
           @keydown.up.prevent="pointerBackward()"
           @keydown.enter.prevent.stop.self="addPointerElement($event)"
-          @keydown.tab.stop.self="addPointerElement($event)"
+          @keydown.tab.prevent.stop.self="addPointerElement($event)"
           @keydown.delete.stop="removeLastElement()"
           class="multiselect__input"/>
         <span
